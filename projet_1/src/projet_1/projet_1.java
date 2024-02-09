9114555b076c5910b7950c63af74aafc2da07890
FUJITSU <FUJITSU@Fujitsu_U748>
2024-02-09 10:57:41 +0100

    Initial Commit.

---
+++ projet_1/src/projet_1/projet_1.java
@@ -0,0 +1,41 @@
+package projet1;
+import java.util.Scanner;
+
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Entrez le nombre a=" ); 
+		Scanner sc= new Scanner(System.in);
+		int a;
+		
+	    a = sc.nextInt(); 
+	    
+		int secret_nbr=22;
+		
+		while(a!=secret_nbr) {
+			
+			if(a>secret_nbr) {
+				System.out.println("le némuro que vous avez saisi est sépurieur au némuro sucret" );
+				System.out.println("Entrez le nombre a=" ); 
+				a = sc.nextInt(); 
+			}
+			else if(a<secret_nbr) {
+				System.out.println("le némuro que vous avez saisi est inférieur au némuro sucret" );
+				System.out.println("Entrez le nombre a=" ); 
+				a = sc.nextInt(); 
+				
+			
+		}
+
+		}
+	   System.out.println("bravo vous avez gagné le jeux" );
+			
+		
+		
+
+	}
+
+}
